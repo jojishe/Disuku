979bda5717f449bbe19d46ab207dec42cb41f63f
dongmucat <1127093059@qq.com>
2025-01-10 10:23:36 +0800

    feat: test

--- frontend/web-app/src/pages/home/index.tsx
+++ frontend/web-app/src/pages/home/index.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + TypeScirpt + ESlint + Prettier + SWC</h1>
+      <h1>Vite + React + TypeScirpt + ESlint + Prettier + SWC 1111</h1>
       <div className="card">
         <button onClick={() => increasePopulation()}>count is {bears}</button>
         <p>
